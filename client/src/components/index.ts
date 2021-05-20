60ada6342b9709d1a2518f8b84d9d55a24fed2d4
Liam Cooper <liamcoop1997@gmail.com>
2021-05-19 19:42:30 -0700

    removed reference to components/icons as it was removed in the previous commit.

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,4 +1,3 @@
 export type { Country, CurrenciesEntity, Rates } from './types';
 export { CurrencySelect } from './currencySelect';
 export { NumberInput } from './numberInput';
-export { Swap } from './icons';
